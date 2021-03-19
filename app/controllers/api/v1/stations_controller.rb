11ef5081eb4b88019e69af7b98a14196f1610ec3
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-19 19:01:36 +0900

    Made it searchable

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::StationsController < Api::V1::BaseController
   def index
+    if params[:query].present?
+    @stations = Station.where("name ILIKE ?", "%#{params[:query]}%")
+    else
     @stations = Station.all.order(created_at: :asc)
+    end
   end
 end
