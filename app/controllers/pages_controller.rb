822f15d7bc5c5b219454dee0cb635d41eef95ed3
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-20 10:00:51 +0900

    attempt to delete pundit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
     @stations = Station.all
   end
--- app/policies/station_policy.rb
+++
@@ -1,7 +0,0 @@
-class StationPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
-  end
-end
