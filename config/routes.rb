fe35880a8a649402ded6c7a42bdbd831df18115e
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-20 15:58:33 +0900

    Better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'lines/display'
+  get 'lines/', to: "lines#display"
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
