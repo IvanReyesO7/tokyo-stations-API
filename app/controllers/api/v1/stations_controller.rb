731111f1230bdefb1d7c7d045592e580a7e3e108
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-21 15:15:54 +0900

    Fixed bug in Heroku

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::StationsController < Api::V1::BaseController
   def index
     if params[:query].present?
-      sql_query = "name ILIKE :query OR name_kanji ILIKE :query OR name_kana ILIKE :query"
+      sql_query = "name_romaji ILIKE :query OR name_kanji ILIKE :query OR name_kana ILIKE :query"
       @stations = Station.where(sql_query, query: "%#{params[:query]}%")
     else
       @stations = Station.all.order(created_at: :asc)
